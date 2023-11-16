34889703c7be3a53fe9b9ce4eaaa94a34cd29cff
Roysandrone1998 <raysandrone@gmail.com>
2023-11-16 01:26:58 -0300

    trabajando

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
-const express = require("express")
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("<h1>hola mundo desde express<h1>");
+});
+
+app.listen(8080, () => console.log("server listening on port 8080"));
