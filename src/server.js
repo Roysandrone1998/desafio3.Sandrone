b09b0ac0832a0cf3b29e2767ef414e59d14d0e38
Roysandrone1998 <raysandrone@gmail.com>
2023-12-05 22:38:37 -0300

    Corrigiendo

--- src/server.js
+++ src/server.js
@@ -1,9 +1,33 @@
-import express from "express";
+import express from 'express';
+import ProductManager from './productManager.js';
 
 const app = express();
+const port = 3000;
 
-app.get("/", (request, response) => {
-  response.send("<h1>hola mundo desde express<h1>");
+
+const manager = new ProductManager('./Productos.json');
+
+app.get('/products', async (req, res) => {
+  const limit = parseInt(req.query.limit, 10);
+
+  if (isNaN(limit)) {
+    res.json(manager.consultarProductos());
+  } else {
+    res.json(manager.consultarProductos().slice(0, limit));
+  }
 });
 
-app.listen(8080, () => console.log("server listening on port 8080"));
+app.get('/products/:pid', (req, res) => {
+  const productId = parseInt(req.params.pid, 10);
+  const product = manager.getProductById(productId);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ error: 'Producto no encontrado' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
